1e01c003c294aff701a2fda250362d736941826c
joseleon1903 <joseleon1903@outlook.com>
2019-08-29 23:31:27 -0400

    creation project endpoint

--- Heliopausa/src/main/java/com/voyager/app/rest/controller/closed/ProjectController.java
+++ Heliopausa/src/main/java/com/voyager/app/rest/controller/closed/ProjectController.java
@@ -1,12 +1,21 @@
 package com.voyager.app.rest.controller.closed;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.voyager.app.rest.dto.ProjectDto;
+import com.voyager.app.security.auth.JwtAuthenticationToken;
+import com.voyager.app.security.model.UserContext;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/project")
 public class ProjectController {
 
+    @PostMapping("/creation")
+    public ResponseEntity creationProject(@RequestBody ProjectDto projectDto) {
+
+
+        return ResponseEntity.ok().build();
+    }
 
 
 }
---
+++ Heliopausa/src/main/java/com/voyager/app/rest/dto/ProjectDto.java
@@ -0,0 +1,10 @@
+package com.voyager.app.rest.dto;
+
+import lombok.Data;
+
+/**
+ * Created by jose on 8/29/2019.
+ */
+@Data
+public class ProjectDto {
+}
